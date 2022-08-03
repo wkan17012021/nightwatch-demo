e40fee65e0c8b0f318dad023a88cc27335f38498
William Kan <william.kan@oneyoungworld.com>
2022-08-03 11:39:40 +0100

    attempt scholarships page obj

---
+++ pages/scholarships.js
@@ -0,0 +1,11 @@
+module.exports = {
+    url: 'https://www.oneyoungworld.com/scholarships',
+    elements: {
+        gdprbtn: { 
+            'selector': '#gdpr-agree'
+        },
+        faqTitle: { 
+            'selector': '#accordion > div'
+        }
+    }
+}
--- tests/scholarships.test.js
+++ tests/scholarships.test.js
@@ -1,19 +1,31 @@
 module.exports = {
     "User can navigate to scholarships page and open faq accordion": function (browser) {
-        //.execute() is not considered a function when chained to page obj. Place all selectors and URL in the test page instead. Execute() is required to scroll the page down so the faq accordion is visible in the screen. The coordinates are trial & error- Nightwatch should have a method that auto scrolls the element into view.  
-        const gdprbtn = '#gdpr-agree';
+        //.execute() is not considered a function when chained to page obj. Place all selectors and URL in the test page instead. Execute() is required to scroll the page down so the faq accordion is visible in the screen. The coordinates are trial & error- Nightwatch should have a method that auto scrolls the element into view.
+        var scholarship = browser.page.scholarships();
+
+        // const gdprbtn = '#gdpr-agree';
         const faqTitle = '#accordion > div';
 
-        browser
-        .url('https://www.oneyoungworld.com/scholarships')
-        .pause(2000)
-        .waitForElementVisible(gdprbtn)
-        .click(gdprbtn)
-        .execute('scrollTo(500, 11000)')
+        scholarship.navigate()
+        // .waitForElementVisible('@gdprbtn')
+        // .click('@gdprbtn');
+
+        scholarship.api.execute('scrollTo(500, 11000)')
         .pause(2000)
         .waitForElementVisible(faqTitle)
         .click(faqTitle)
-        .pause(2000)
+        .pause(2000);
+
+        // browser
+        // .url('https://www.oneyoungworld.com/scholarships')
+        // .pause(2000)
+        // .waitForElementVisible(gdprbtn)
+        // .click(gdprbtn)
+        // .execute('scrollTo(500, 11000)')
+        // .pause(2000)
+        // .waitForElementVisible(faqTitle)
+        // .click(faqTitle)
+        // .pause(2000)
     }
     
 }
